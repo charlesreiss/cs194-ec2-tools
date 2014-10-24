c86d33ec59eb7803eecc02bf9102e4948969da3a
Charles Reiss <woggle@apache.org>
2014-10-23 21:41:47 -0700

    Rename start -> run

--- start_instances.py
+++ start_instances.py
@@ -8,11 +8,11 @@ import account_util
 
 ## startstop_instances.py --mode={start,stop} --ami=ami --type=type
 def setup_args(parser):
-    parser.add_argument('--mode', choices=['start','stop','untag','retag','terminate'], required=True)
+    parser.add_argument('--mode', choices=['run','stop','untag','retag','terminate'], required=True)
     parser.add_argument('--ami', default='ami-b5a7ea85') # US West Oregon, HVM, 64-bit, Amazon Linux AMI
     parser.add_argument('--type', default='t2.micro')
 
-def start_instances(args):
+def run_instances(args):
     user_list = account_util.get_users(args)
     ec2 = account_util.connect_ec2(args)
     existing_instances = account_util.instances_by_user(args, ec2)
@@ -36,11 +36,11 @@ def start_instances(args):
     return failed_instances.keys()
 
 def stop_instances(args):
+    user_set = set(account_util.get_users(args))
     ec2 = account_util.connect_ec2(args)
     for user, instances in account_util.instances_by_user(args, ec2, user_set=user_set).iteritems():
-        if user not in user_set:
-            continue
         for instance in instances:
+            logging.warning('Stopping %s', instance)
             instance.stop()
 
 def terminate_instances(args):
@@ -48,6 +48,7 @@ def terminate_instances(args):
     ec2 = account_util.connect_ec2(args)
     for user, instances in account_util.instances_by_user(args, ec2, user_set=user_set).iteritems():
         for instance in instances:
+            logging.warning('Terminating %s', instance)
             instance.terminate()
 
 def untag_instances(args):
@@ -67,11 +68,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
     account_util.init_logging(args)
 
-    if args.mode == 'start':
+    if args.mode == 'run':
         if args.ami is None:
             logging.fatal('Need to specify AMI')
             sys.exit(1)
-        failed = start_instances(args)
+        failed = run_instances(args)
         if len(failed):
             logging.error('Failed to start instances for %s', sorted(failed))
     elif args.mode == 'untag':
